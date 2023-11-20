909f77d928da697acd536c3f9a290a5a5a0bf48b
Ana Brunner <anabrunner.ab@gmail.com>
2023-11-19 20:33:56 -0800

    Add fetch function for random word API.

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,23 @@ const $howToPlayBtn = $('#howToPlayBtn');
 const $newGameBtn = $('#newGameBtn');
 const $instructions = $('#instructions');
 const $closeInstructionsBtn = $('#closeInstructions');
+const fetchWordURL = 'https://www.wordgamedb.com/api/v1/words/random';
 
 $howToPlayBtn.click(() => $instructions.show(300));
 $closeInstructionsBtn.click(() => $instructions.hide(300));
+$newGameBtn.click(restartGame);
+
+function restartGame(){
+  fetch(fetchWordURL)
+    .then(function(response){
+      if(response.ok){
+        return response.json();
+      }
+    })
+    .then(function(data){
+      console.log(data.word);
+    })
+    .catch(function(){
+      console.log('Catch fetch error');
+    });
+};
