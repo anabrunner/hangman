9025e57554bef6ebbee1dd27f2126e565b64e2bd
Ana Brunner <anabrunner.ab@gmail.com>
2023-11-19 21:20:19 -0800

    Add letters and hint display when word is fetched.

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,17 @@
 
 const $howToPlayBtn = $('#howToPlayBtn');
 const $newGameBtn = $('#newGameBtn');
+const $wordDiv = $('#word');
+const $hintDiv = $('#hint');
 const $instructions = $('#instructions');
 const $closeInstructionsBtn = $('#closeInstructions');
 const fetchWordURL = 'https://www.wordgamedb.com/api/v1/words/random';
 
 $howToPlayBtn.click(() => $instructions.show(300));
 $closeInstructionsBtn.click(() => $instructions.hide(300));
-$newGameBtn.click(restartGame);
+$newGameBtn.click(getRandomWord);
 
-function restartGame(){
+function getRandomWord(){
   fetch(fetchWordURL)
     .then(function(response){
       if(response.ok){
@@ -19,8 +21,20 @@ function restartGame(){
     })
     .then(function(data){
       console.log(data.word);
+      console.log(data.word.length);
+      $hintDiv.text(`HINT: ${data.hint}`);
+      makeWordDiv(data.word);
     })
     .catch(function(){
       console.log('Catch fetch error');
     });
+};
+
+function makeWordDiv(word){
+  $wordDiv.empty();
+  let n = word.length;
+  for (let i = 1; i <= n; i++){
+    const $letterDiv = $('<div></div>');
+    $wordDiv.append($letterDiv);
+  }
 };
