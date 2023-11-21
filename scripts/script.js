7ca43f2ab3afc0a3f120a363846be0baba2792f5
Ana Brunner <anabrunner.ab@gmail.com>
2023-11-20 20:30:34 -0800

    Add function to make letter keys.

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,11 @@
 
 const $howToPlayBtn = $('#howToPlayBtn');
 const $newGameBtn = $('#newGameBtn');
+const $hangmanImg = $('#hangmanImg');
 const $wordDiv = $('#word');
 const $hintDiv = $('#hint');
+const $scoreDiv = $('#score');
+const $letterKeys = $('#letterKeys');
 const $instructions = $('#instructions');
 const $closeInstructionsBtn = $('#closeInstructions');
 const fetchWordURL = 'https://www.wordgamedb.com/api/v1/words/random';
@@ -11,6 +14,18 @@ const fetchWordURL = 'https://www.wordgamedb.com/api/v1/words/random';
 $howToPlayBtn.click(() => $instructions.show(300));
 $closeInstructionsBtn.click(() => $instructions.hide(300));
 $newGameBtn.click(getRandomWord);
+
+makeLetterKeys();
+
+// Creates buttons for letters
+function makeLetterKeys(){
+  let i = 65;
+  while (i <= 90){
+    let letter = String.fromCharCode(i);
+    $letterKeys.append(`<button id="${letter}">${letter}</button>`);
+    i++;
+  }
+};
 
 function getRandomWord(){
   fetch(fetchWordURL)
@@ -22,7 +37,7 @@ function getRandomWord(){
     .then(function(data){
       console.log(data.word);
       console.log(data.word.length);
-      $hintDiv.text(`HINT: ${data.hint}`);
+      $hintDiv.text(`HINT: ${data.category} - ${data.hint}`);
       makeWordDiv(data.word);
     })
     .catch(function(){
